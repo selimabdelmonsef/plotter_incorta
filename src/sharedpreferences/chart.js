c11a63ceb1d2d8e0327ff84473567edae1f314bd
Selim <selimabdelmonsef@gmail.com>
2021-05-25 05:50:01 +0200

    fixed styles

--- src/sharedpreferences/chart.js
+++ src/sharedpreferences/chart.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-// import styles from './plotter_page.module.css'
 import { connect } from "react-redux";
 import { _GetPlotterData } from "../redux/redux-actions/plotter_data_action";
 import { Line } from "react-chartjs-2";
@@ -12,29 +11,8 @@ class Chart extends React.Component {
             labels: [],
             datasets: 
             [
-                // {
-                //     label: "My First dataset",
-                //     fill: true,
-                //     lineTension: 0.3,
-                //     backgroundColor: "rgba(225, 204,230, .3)",
-                //     borderColor: "rgb(205, 130, 158)",
-                //     borderCapStyle: "butt",
-                //     borderDash: [],
-                //     borderDashOffset: 0.0,
-                //     borderJoinStyle: "miter",
-                //     pointBorderColor: "rgb(205, 130,1 58)",
-                //     pointBackgroundColor: "rgb(255, 255, 255)",
-                //     pointBorderWidth: 10,
-                //     pointHoverRadius: 5,
-                //     pointHoverBackgroundColor: "rgb(0, 0, 0)",
-                //     pointHoverBorderColor: "rgba(220, 220, 220,1)",
-                //     pointHoverBorderWidth: 2,
-                //     pointRadius: 1,
-                //     pointHitRadius: 10,
-                //     data: [70, 59, 80, 81, 56, 55, 40]
-                // },
                 {
-                    label: "Dimensions Vs Measures",
+                    label: "Measures vs Dimensions",
                     fill: true,
                     lineTension: 0.1,
                     backgroundColor: "rgba(184, 185, 210, .3)",
@@ -59,45 +37,16 @@ class Chart extends React.Component {
     }
   }
        async getDimensionsData (){
-            this.props.plotterData[0].values?.map((dimensionsValue)=>{
-                
+            this.props.plotterData[0].values?.map((dimensionsValue)=>{          
                 this.setState({
                     dataLine:this.state.dataLine = {
                         datasets:this.state.dataLine.datasets,
-                        // datasets:this.state.dataLine.datasets,
                         labels:[...this.state.dataLine.labels,dimensionsValue]}
-
                 })
             })
-            console.log(this.props.plotterData)
-            // console.log(this.state.dataLine.datasets[0].data)        
-       
+            console.log(this.props.plotterData)       
         }
         async getMeasures(){
-            // this.props.plotterData[1].values.map((measuresValue,index)=>{
-            //     this.setState({
-            //         dataLine:this.state.dataLine = {
-            //             labels:this.state.dataLine.labels,
-            //             // datasets:this.state.dataLine.datasets,
-            //             datasets:[...this.state.dataLine.datasets[0]?.data[index],3]}
-                        
-
-            //     })
-            // })
-            // this.setState({
-            //     dataLine:this.state.dataLine = {
-            //         labels:this.state.dataLine.labels,
-            //         // datasets:this.state.dataLine.datasets,
-            //         datasets:[...this.state.dataLine.datasets[0]?.data,"3"]}
-
-            // })
-                //   this.setState({
-                //     dataLine:this.state.dataLine = {
-                //         labels:this.state.dataLine.labels,
-                //         // datasets:this.state.dataLine.datasets,
-                //         datasets:[...this.state.dataLine.datasets[0].data,22]}
-
-                // })
                 this.props.plotterData[1].values.map((measuresValue,index)=>{
                 this.state.dataLine.datasets.forEach((dataset) => {
                     dataset.data.push(parseInt(this.props.plotterData[1].values[index]));
@@ -115,10 +64,6 @@ class Chart extends React.Component {
             this.getDimensionsData();
             
     }, 2000)
-        // this.setState({
-
-        // })
-        // console.log(this.state.dimensions);
     }
     render() {
         return (
